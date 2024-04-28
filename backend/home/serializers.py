606544807bc07a70fe79a7c1fbfe66e95e3f4bbf
Devanshee-Gupta <devansheegupta0@gmail.com>
2024-04-28 23:59:54 +0530

    serializers added

---
+++ backend/home/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework.serializers import ModelSerializer
+from home.models import *
+
+class UsersSerializer(ModelSerializer):
+    class Meta:
+        model = Users
+        fields = '__all__'
+
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class SavedCollectionSerializer(ModelSerializer):
+    class Meta:
+        model = SavedCollection
+        fields = '__all__'
+
+class SavedItemSerializer(ModelSerializer):
+    class Meta:
+        model = SavedItem
+        fields = '__all__' 
